fec309aae2fa07a7f3dacf2a90a6eb7dab960322
Ksenia Kuntsevich <kse.kuntsevich@icloud.com>
2019-09-29 20:11:15 +0300

    feat: add bracket task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  if(str.length % 2) return false;
+
+  let keys = {};
+
+  bracketsConfig.forEach(config => keys[config[0]] = config[1] );
+
+  let sequence = [];
+
+  str.split('').forEach((bracket) => {
+    if(bracket === keys[sequence[sequence.length - 1]]) {
+        sequence.pop();
+    } else {
+        sequence.push(bracket);
+    }
+
+  })
+
+  return sequence.length === 0;
 }
